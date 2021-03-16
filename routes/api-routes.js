53dae9b2de7baaaaf8a8147f34b5d8c1372604cc
Ray Luna <leon.luna.ray@gmail.com>
2021-03-15 20:01:43 -0700

    html/api routes working

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,28 +2,81 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Workout = require('../models/workout.js');
 
-const router = express.Router();`
+const router = express.Router();
 
 router.post('/api/workouts', ({ body }, res) => {
-  Transaction.create(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+  Workout.create(body)
+    .then(data => {
+      res.json(data);
     })
     .catch(err => {
       res.status(400).json(err);
     });
 });
 
+// workout data comes from the body, id from the params
+router.put('/api/workouts/:id', ({body, params}, res) => {
+    console.log(params)
+    Workout.findByIdAndUpdate(
+        // This does not need req since it is entered as body and params in the argument
+        params.id, {
+            $push: {
+                exercises: body
+            }
+        }, { new: true}
+        // new: true will update the info in the db
+    ).then(data => res.json(data))
+    .catch(err => {
+        res.status(400).json(err);
+      });
+});
 
 router.get('/api/workouts', (req, res) => {
-  Transaction.find({})
+  Workout.aggregate([{
+      $addFields: {
+        totalDuration: {
+            // Dynamically create column for total duration with sum of exercies @ duration key
+            $sum: '$exercises.duration' ,
+        }
+      }
+  },])
     .sort({ date: -1 })
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+    .then(data => {
+      res.json(data);
     })
     .catch(err => {
       res.status(400).json(err);
     });
 });
+
+router.get('/api/workouts/range', (req, res) => {
+    Workout.aggregate([{
+        $addFields: {
+          totalDuration: {
+              // Dynamically create column for total duration with sum of exercies @ duration key
+              $sum: '$exercises.duration' ,
+          }
+        }
+    },]).limit(10)
+    .then(data => res.json(data))
+    .catch(err => {
+        res.status(400).json(err);
+      });;
+})
+
+// Aggregate function to get the total duration for each workout.
+
+// db.scores.aggregate( [
+//     {
+//       $addFields: {
+//         totalHomework: { $sum: "$homework" } ,
+//         totalQuiz: { $sum: "$quiz" }
+//       }
+//     },
+//     {
+//       $addFields: { totalScore:
+//         { $add: [ "$totalHomework", "$totalQuiz", "$extraCredit" ] } }
+//     }
+//  ] )
 
 module.exports = router;
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const db = require('../models');
 
 const router = express.Router();
 
 router.get ('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/index.html')); 
+    res.sendFile(path.join(__dirname, '../public/index.html')); 
 });
+
+router.get ('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html')); 
+});
+
+router.get ('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html')); 
+});
+
+module.exports = router;
